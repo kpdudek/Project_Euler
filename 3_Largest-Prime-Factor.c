976edf7328403761c972356048362734fd3d9167
kurt dudek <kpdudek@bu.edu>
2019-02-04 08:23:42 -0500

    Problem 3 & 4 partially completed

--- 3_Largest-Prime-Factor.c
+++ 3_Largest-Prime-Factor.c
@@ -3,33 +3,58 @@
 
 // Project Euler Problem Numer
 /*
-
+	The prime factors of 13195 are 5, 7, 13 and 29.
+	What is the largest prime factor of the number 600851475143 ?
 */
 
+double modulus(double val,double div) {
+	double sum=0,remainder;
+	while(1){
+		sum = sum + div;
+		if((val - sum) < div){
+			remainder = val - sum;
+			break;
+		}
+		else if((val - sum) == 0){
+			remainder = 0;
+			break;
+		}
+	}
+	return remainder;
+}
 
-int is_prime(val) {
-	for(double i = val-1; i > 0; i--) {
-		int factor = val%i;
+double is_prime(double val) {
+	double fact;
+	for(double i = 2; i < (val/2); i++) {
+		fact = val/i;
+		double factor = fmod(val,fact);
 		if(factor == 0){
 			return 0;
 		}
+		printf("%f\n",i);
 	}
 	return 1;
 }
 
 
 double Largest_Prime(double val) {
-	for(double i=val-1; i > 0; i--) {
-		int factor = val%i;
-		if((factor == 0) && is_prime(i)){
-			return i;
+	double lpf,fact;
+	for(double i=2; i < (val/2); i++) {
+		fact = val/i;
+		double factor = fmod(val,fact);
+		if((factor == 0) && is_prime(fact)){
+			lpf = fact;
+			break;
 		}
+		printf("%f\n",fact);
 	}
+	return lpf;
 }
 
 
-void main(){
+double main(){
 	double pass = 600851475143;
 	double ans = Largest_Prime(pass);
 	printf("The anser is: %f\n",ans);
+	return 0;
 }
---
+++ 4_Largest_Palindrom_Product.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+// Project Euler Problem Numer
+/*
+  A palindromic number reads the same both ways. The largest palindrome made
+  from the product of two 2-digit numbers is 9009 = 91 × 99.
+  Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+int palindrome() {
+  int num1,num2,prod;
+  for(int i;i > 0;i--){
+    for(int j;j > 0;j--){
+      prod = (num1-i)*(num2-j);
+    }
+  }
+}
+
+int main(){
+
+
+	return 0;
+}
