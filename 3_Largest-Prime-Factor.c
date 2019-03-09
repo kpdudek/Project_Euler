a50f1ab1598ddcf97b5d61c9c3760b1107114952
Kurt <kpdudek2@gmail.com>
2019-03-09 10:29:25 -0500

    Commented #3

--- 3_Largest-Prime-Factor.c
+++ 3_Largest-Prime-Factor.c
@@ -7,6 +7,10 @@
 	What is the largest prime factor of the number 600851475143 ?
 */
 
+
+// This function checks if the number passed to it is prime
+//	returns 1 if prime
+//	returns 0 if non-prime
 int Is_Prime(unsigned long long int number) {
     if (number <= 3 && number > 1)
         return 1;            // as 2 and 3 are prime
@@ -22,26 +26,38 @@ int Is_Prime(unsigned long long int number) {
     }
 }
 
+
+// This function finds the largest prime factor of the numer passed to it
+//	calls Is_Prime()
 unsigned long long int Largest_Prime(unsigned long long int val) {
 	unsigned long long int lpf,fact;
+	// Check if values counting up from 3 are factors. The iteration step
+	// is two because any even number will not be prime
 	for(unsigned long long int i=3; i<val; i+=2)
 	{
+		// check if the value is a factor
 		fact = val%i;
+		// If modulus is 0, the value is a factor
 		if (fact == 0)
 		{
+			// Print the value
 			printf("%lld\n",i);
+
+			// If the number is a factor, find the number of times it goes into val
+			// num is the largest value that is a factor of val
 			unsigned long long int num = val/i;
+			
+			// Check if num is prime
 			if (Is_Prime(num))
 			{
-				lpf = num;
-				break;
+				return num;				
 			}
 		}
 	}
-	return lpf;
 }
 
 
+// Find the Largest Prime Factor
 unsigned long long int main(){
 	unsigned long long int pass = 600851475143;
 	unsigned long long int ans = Largest_Prime(pass);
