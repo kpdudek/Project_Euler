c92bd2f9b69de79635d91fb48639ceda0b99a968
k-dudek <kdudek@rtr.ai>
2019-07-09 16:32:04 -0400

    Problem 4

--- 04_Largest_Palindrome_Product.c
+++ 04_Largest_Palindrome_Product.c
@@ -10,31 +10,31 @@
   Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
-int is_even(long int val)
+int is_even(unsigned long int val)
 {
   if (val%2 == 0){return 1;}
   else{return 0;}
 }
 
 
-long int palindrome(long int num) {
-  long int num1,num2,prod;
+long int palindrome(unsigned long int num) {
+  unsigned long int num1,num2,prod;
   char str[20];
 
-  for (long int i=num;i>0;i--)
+  for (unsigned long int i=num;i>0;i--)
   {
-    for (long int j=num;j>0;j--)
+    for (unsigned long int j=num;j>0;j--)
     {
       prod = i*j;
       //printf("%ld",prod);
       sprintf(str,"%ld",prod);
-      long int len = strlen(str);
+      unsigned long int len = strlen(str);
       //printf("%ld\n",len);
       //printf("%c %c\n",str[0],str[len-1]);
       if (is_even(len))
       {
         int pal = 1;
-        for (int i=0;i<len/2;i++)
+        for (int i=0;i<(len/2)-1;i++)
         {
           if (str[i] != str[len-1-i])
           {
@@ -47,7 +47,7 @@ long int palindrome(long int num) {
       else
       {
         int pal = 1;
-        for (int i=0;i<((len-1)/2);i++)
+        for (int i=0;i<(((len-1)/2)-1);i++)
         {
           if (str[i] != str[len-1-i])
           {
@@ -63,8 +63,8 @@ long int palindrome(long int num) {
 
 
 long int main(){
-  long int val = 999;
-  long int out = palindrome(val);
+  unsigned long int val = 999;
+  unsigned long int out = palindrome(val);
   printf("The answer is: %ld\n",out);
 	return 0;
 }
